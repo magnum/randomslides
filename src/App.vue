e2f7ad1b5fa6940346abef8e2d237f70d1cf9109
Antonio Molinari <antoniomolinari@me.com>
2026-02-16 19:04:08 +0100

    setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <main class="flex flex-col items-center justify-center h-screen w-screen">
+    <h1 class="text-[8vw] font-bold">Random Slides</h1>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
