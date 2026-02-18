e44bbca0c56dfb24fd5aa4abbb7574b256dbb4ca
Antonio Molinari <antoniomolinari@me.com>
2026-02-18 08:25:06 +0100

    Enhance index.html with Google Fonts integration and update App.vue to include a predefined items array for slides. Modify Slides.vue to implement a spin wheel feature for random slide selection, improving user interaction. Update style.css to apply the Roboto Slab font.

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,53 @@
 import { ref } from 'vue';
 import Slides from './components/Slides.vue'
 
-const items = ref(
-  Array.from({ length: 10 }, (_, index) => index + 1)
-  .map(index => ({
-    id: index,
-    title: `Slide ${index}`,
-    content: `This is the ${index} slide`,
-  }))
-);
+const items = ref([
+  {
+    id: 1,
+    title: 'Probabilistic',
+    content: 'content',
+  },
+  {
+    id: 2,
+    title: 'Learning',
+    content: 'content',
+  },
+  {
+    id: 3,
+    title: 'Skill',
+    content: 'content',
+  },
+  {
+    id: 4,
+    title: 'Inefficiency',
+    content: 'content',
+  },
+  {
+    id: 5,
+    title: 'Agent',
+    content: 'content',
+  },
+  {
+    id: 6,
+    title: 'Human',
+    content: 'content',
+  },
+  {
+    id: 7,
+    title: 'Ai slop',
+    content: 'content',
+  },
+  {
+    id: 8,
+    title: "Jobs",
+    content: 'content',
+  },
+  {
+    id: 9,
+    title: 'rag',
+    content: 'content',
+  },
+]);
 
 </script>
 
--- src/components/Slides.vue
+++ src/components/Slides.vue
@@ -1,15 +1,47 @@
 <script setup>
 import { ref } from 'vue';
 
-defineProps({
+const props = defineProps({
   items: {
     type: Array,
     required: true,
   },
 });
 
+let currentItem = ref();
+let nextTimeout;
+let timeoutIncrement = 8;
+let cycleCount;
+const spinWheel = () => {
+  nextTimeout = 10
+  cycleCount = 20;
+  cycle();
+};
+const cycle = () => {
+  nextTimeout += timeoutIncrement*(10/cycleCount);
+  //console.log(nextTimeout);
+  const randomIndex = Math.floor(Math.random() * props.items.length);
+  currentItem.value = props.items[randomIndex];
+  if (cycleCount == 0) {
+    console.log('cycle complete');
+  } else {
+    cycleCount -= 1;
+    setTimeout(cycle, nextTimeout);
+  }
+};
 </script>
 
 <template>
-  <h1 class="text-[8vw] font-bold">Random Slides</h1>
+  <a 
+    class="
+      text-[8vw] 
+      font-bold text-center max-w-[70vw] mx-auto leading-[1.4] mb-10 
+      cursor-pointer 
+      transition-all duration-100
+    " 
+    href="#"
+    @click="spinWheel"
+  >
+    <mark class=" bg-gray-100 p-4 hover:bg-gray-900 hover:text-white">{{ currentItem?.title || 'Click to spin the wheel!' }}</mark>
+  </a>
 </template>
